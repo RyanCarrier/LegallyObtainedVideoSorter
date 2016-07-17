c9d1a78f25fb0abf530ec7c3a3938eb1147aac94
rcarrier <rcarrier@byteback.com.au>
2016-07-17 21:11:19 +1000

    actually fixed it lmao

--- sort.py
+++ sort.py
@@ -3,7 +3,6 @@ from os import walk
 import shutil
 import os
 import re
-from time import sleep
 
 pwd = os.getcwd()
 
@@ -22,30 +21,33 @@ def fixAndCheck(folder):
     return folder
 
 
-def update_progress(progress,current):
-	if 52+4+len(current)>80:
-		current=current[:80-52-54]
-    print '\r[{0}{1}] {2}%'.format('#' * (progress / 2),
-                                   ' ' * ((100 - progress) / 2), progress), current,
+def update_progress(progress, current):
+    if 52 + 4 + len(current) > 80:
+        current = current[:80 - 52 - 54]
+    print '\r[{0}{1}] {2}% - {3}'.format('#' * (progress / 2),
+                                         ' ' * ((100 - progress) /
+                                                2), progress, current),
     os.sys.stdout.flush()
 
 
 default = "./"
-folder = raw_input("Enter source folder: %s"%default + chr(8)*len(default))
+folder = raw_input("Enter source folder: %s" % default + chr(8) * len(default))
 if not folder:
-	folder = default
+    folder = default
 folder = fixAndCheck(folder)
 
 default = "../"
-targetfolder = raw_input("Enter source folder: %s"%default + chr(8)*len(default))
+targetfolder = raw_input("Enter source folder: %s" % default + chr(8) * len(
+    default))
 targetfolder = fixAndCheck(targetfolder)
 response = raw_input("Copy, Move or dry run: ")
-action = 0  #dry run
+action = 0  # dry run
+
 if len(response) > 0:
     if response[0] == 'c' or response[0] == 'C':
-        action = 1  #copy
+        action = 1  # copy
     elif response[0] == 'm' or response[0] == 'M':
-        action = 2  #copy
+        action = 2  # copy
 
 filenames = []
 for (dirpath, dirnames, f) in walk(folder):
@@ -55,14 +57,12 @@ for (dirpath, dirnames, f) in walk(folder):
 regex = re.compile(r'(.*)(\b[s, S]\d{2}[e, E]\d{2})', re.IGNORECASE)
 i = 0
 for f in filenames:
-    update_progress(100 * i / len(filenames))
-    i += 1
-    #nobody likes regex, or low quality videos
-    if ("720p" in f or "720P" in f or "1080p" in f or "1080P" in f) and ".mkv" in f:
 
+    # nobody likes regex, or low quality videos
+    if ("720p" in f or "720P" in f or "1080p" in f or
+            "1080P" in f) and ".mkv" in f:
         show, seasonep = regex.match(f).groups()
         showstr = show.strip('.').replace('.', ' ')
-
         season = seasonep[1:3]
         ep = seasonep[-2:]
         if season[0] == '0':
@@ -73,8 +73,10 @@ for f in filenames:
             continue
         if not os.path.exists(path):
             os.makedirs(path)
+        update_progress(100 * i / len(filenames), showstr + " " + seasonep)
         if action == 1:
             shutil.copy(folder + '/' + f, path)
         else:
-            os.rename(folder + '/' + f, path+'/'+f)
-update_progress(100)
+            os.rename(folder + '/' + f, path + '/' + f)
+    i += 1
+update_progress(100, f)
