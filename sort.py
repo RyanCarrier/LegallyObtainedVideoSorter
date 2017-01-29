38d2652e7b78c2765d1b3aa04b76c61a250fb4ad
Ryan Carrier <ryan.j.carrier@gmail.com>
2017-01-30 00:51:30 +1100

    added if no qual

--- sort.py
+++ sort.py
@@ -59,6 +59,8 @@ i = 0
 for f in filenames:
     i += 1
     if ("HDTV" in f or "720p" in f or "720P" in f or "1080p" in f or "1080P" in f) and (".mkv" in f or ".mp4" in f or ".avi" in f):
+        if regex.match(f) is None:
+            continue
         show, seasonep = regex.match(f).groups()
         showstr = show.strip('.').replace('.', ' ')
         season = seasonep[1:3]
